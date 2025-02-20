ad0ae9a9587616e9f5392acc7e21adc0b7459ead
SamirValiyev <vliyevsamir5@gmail.com>
2025-02-20 14:52:24 +0400

    UnitOfWork Created

--- Infrastructure/EcommerceApiSrc.Persistence/Registration.cs
+++ Infrastructure/EcommerceApiSrc.Persistence/Registration.cs
@@ -1,6 +1,8 @@
 ﻿using EcommerceApiSrc.Application.Interfaces.Repositories;
+using EcommerceApiSrc.Application.Interfaces.UnitOfWorks;
 using EcommerceApiSrc.Persistence.Context;
 using EcommerceApiSrc.Persistence.Repositories;
+using EcommerceApiSrc.Persistence.UnitOfWorks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,6 +18,7 @@ namespace EcommerceApiSrc.Persistence
             serviceCollection.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("ConStr")));
             serviceCollection.AddScoped(typeof(IReadRepository<>),typeof(ReadRepository<>));
             serviceCollection.AddScoped(typeof(IWriteRepository<>),typeof(WriteRepository<>));  
+            serviceCollection.AddScoped<IUnitOfWork,UnitOfWork>();
 
         }
     }
---
+++ Infrastructure/EcommerceApiSrc.Persistence/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using EcommerceApiSrc.Application.Interfaces.Repositories;
+using EcommerceApiSrc.Application.Interfaces.UnitOfWorks;
+using EcommerceApiSrc.Persistence.Context;
+using EcommerceApiSrc.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceApiSrc.Persistence.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async ValueTask DisposeAsync() => await  _appDbContext.DisposeAsync();
+        public int Save() => _appDbContext.SaveChanges();
+        public async Task<int> SaveAsync() => await _appDbContext.SaveChangesAsync();
+         IReadRepository<T> IUnitOfWork.GetReadRepository<T>() => new ReadRepository<T>(_appDbContext);
+        IWriteRepository<T> IUnitOfWork.GetWriteRepository<T>() => new WriteRepository<T>(_appDbContext);
+        
+    }
+}
